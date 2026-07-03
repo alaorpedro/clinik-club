282a54e9b128cc6155db4cd1665bd0001333de53
Alaor Pedro <alaorpedro@Alaor-Pedro-3.local>
2026-07-03 19:02:33 -0300

    bump site version to 1.10

--- src/components/site/SiteFooter.tsx
+++ src/components/site/SiteFooter.tsx
@@ -3,7 +3,7 @@ import { useAuth } from "@/hooks/use-auth";
 import logo from "@/assets/clinik-club-logo.png";
 import icon from "@/assets/clinik-icon.png";
 
-export const APP_VERSION = "1.09";
+export const APP_VERSION = "1.10";
 
 export function SiteFooter() {
   const { user } = useAuth();
